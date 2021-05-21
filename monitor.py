a81d1e65b6e824b06a29d6bd16521c5306d64ee5
Parth Pant <parthpant4@gmail.com>
2021-05-21 18:17:20 +0530

    first commit

---
+++ monitor.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# importing the requests library
+import requests
+import sys
+from datetime import datetime
+  
+date = datetime.now().strftime("%d-/%m-/%Y")
+# api-endpoint
+URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=209&date="+date
+# URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=670&date="+date
+
+HEAD = {
+'Host': 'cdn-api.co-vin.in',
+'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0',
+'Accept': 'application/json, text/plain, */*',
+'Accept-Language': 'en-US,en;q=0.5',
+'Accept-Encoding': 'gzip, deflate, br',
+'Origin': 'https://www.cowin.gov.in',
+'Connection': 'keep-alive',
+'Referer': 'https://www.cowin.gov.in/',
+'TE': 'Trailers'
+}
+  
+r = requests.get(URL, headers=HEAD)
+  
+# extracting data in json format
+  
+response = r.json()
+for center in response['centers']:
+    for session in center['sessions']:
+        if session['min_age_limit'] <= 18 and session['available_capacity'] > 0:
+            if "dharampur" in center['name'].lower() or "dharampur" in center['address'].lower():
+                # print(center['name']+","+center['address'])
+                # print("Age:{}".format(session['min_age_limit']))
+                # print("Date:{}".format(session['date']))
+                # print("Capacity:{}".format(session['available_capacity']))
+                print("VAX")
+                sys.exit()
+
+print("NA")
+
+
